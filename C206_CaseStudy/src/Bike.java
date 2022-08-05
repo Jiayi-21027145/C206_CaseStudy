1db06d2aedfdb343f51eabd5e4b80829db25fbde
21035358 <21035358@LAPTOP-UKQ6BS83>
2022-08-05 16:14:47 +0800

    Firdaus Latest Push

--- C206_CaseStudy/src/Bike.java
+++ C206_CaseStudy/src/Bike.java
@@ -1,4 +1,43 @@
 
-public class Bike {
+public class Bike  {
+	
+	// Yesterday, the bikelisting you guys changed it, This class 
+	//diagram is the new one that I should use.
+	
+	
+	private int Quantity;
+	private String Features;
+	private double Price;
+	
+	public Bike(int quantity, String features, double price) {
+		Quantity = quantity;
+		Features = features;
+		Price = price;
+	}
 
+	public int getQuantity() {
+		return Quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		Quantity = quantity;
+	}
+
+	public String getFeatures() {
+		return Features;
+	}
+
+	public void setFeatures(String features) {
+		Features = features;
+	}
+
+	public double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(double price) {
+		Price = price;
+	}
+	
+	
 }
--- C206_CaseStudy/src/BikeListing.java
+++ C206_CaseStudy/src/BikeListing.java
@@ -1,4 +1,16 @@
 
-public class BikeListing {
+public class BikeListing extends Bike{
 
+	private String bikeName;
+	
+	public BikeListing(int quantity, String features, double price, String bikeName) {
+		super(quantity, features, price);
+		this.bikeName = bikeName;
+	}
+	public String getBikeName() {
+		return bikeName;
+	}
+	public void setBikeName(String bikeName) {
+		this.bikeName = bikeName;
+	}
 }
